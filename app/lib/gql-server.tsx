c0f59530ece7ffe12a99a8136e7db67ebce2be8b
Abhinay Pandey <abhinay.pandey@springy.se>
2025-12-18 10:48:35 +0530

    db url fix

--- app/lib/gql-server.tsx
+++ app/lib/gql-server.tsx
@@ -1,3 +1,5 @@
 import { getGraphQLQuery } from "naystack/graphql/server";
 
-export const query = getGraphQLQuery({ uri: "http://localhost:3000/api" });
+export const query = getGraphQLQuery({
+  uri: process.env.NEXT_PUBLIC_BACKEND_BASE_URL!,
+});
