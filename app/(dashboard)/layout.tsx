e3069572b0eebd5e6aa92eceaeded5b084a7b801
Abhinay Pandey <abhinay.pandey@springy.se>
2026-02-01 01:15:50 +0530

    final

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -3,7 +3,7 @@ import { BottomNavbar } from "./components/bottom-navbar";
 
 export default function layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex pb-[env(safe-area-inset-bottom)] flex-col h-svh">
+    <div className="flex pb-[calc(env(safe-area-inset-bottom)+64px)] flex-col min-h-svh">
       {children}
       <BottomNavbar />
     </div>
