4cbb9d8a10ed7966dbe488f38c7d8f450ff55e2f
Abhinay Pandey <abhinay.pandey@springy.se>
2026-02-10 20:23:02 +0530

    fix

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -3,23 +3,29 @@ import { BottomNavbar } from "./components/bottom-navbar";
 import getCurrentUser from "../api/(graphql)/User/resolvers/get-current-user";
 import { redirect } from "next/navigation";
 import { logout, getCurrentRefreshToken } from "naystack/auth";
+import { Injector } from "naystack/graphql/server";
 
 export default async function layout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const user = await getCurrentUser.authCall();
-  if (!user) {
-    const refreshToken = await getCurrentRefreshToken();
-    if (refreshToken) {
-      await logout();
-    }
-    redirect("/login");
-  }
   return (
     <div className="flex pb-16 flex-col min-h-svh">
       {children}
+      <Injector
+        fetch={async () => {
+          const user = await getCurrentUser.authCall();
+          if (!user) {
+            const refreshToken = await getCurrentRefreshToken();
+            if (refreshToken) {
+              await logout();
+            }
+            redirect("/login");
+          }
+        }}
+        Component={() => null}
+      />
       <div className="h-(--vv-bottom-inset)" />
       <BottomNavbar />
     </div>
