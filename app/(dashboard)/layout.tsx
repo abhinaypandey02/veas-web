53b1970030f845f7a17864e1ef6016686d48ab1e
Abhinay Pandey <abhinay.pandey@springy.se>
2026-02-10 20:06:20 +0530

    fix

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,7 +1,22 @@
 import React from "react";
 import { BottomNavbar } from "./components/bottom-navbar";
+import getCurrentUser from "../api/(graphql)/User/resolvers/get-current-user";
+import { redirect } from "next/navigation";
+import { logout, getCurrentRefreshToken } from "naystack/auth";
 
-export default function layout({ children }: { children: React.ReactNode }) {
+export default async function layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const user = await getCurrentUser.authCall();
+  if (!user) {
+    const refreshToken = await getCurrentRefreshToken();
+    if (refreshToken) {
+      await logout();
+    }
+    redirect("/login");
+  }
   return (
     <div className="flex pb-16 flex-col min-h-svh">
       {children}
